18e1c5bef9e301985046b36bb691b4db8ba6682e
lakhansamani <lakhan.m.samani@gmail.com>
2019-11-01 15:22:01 +0530

    add logic to restart container based on file change

--- service/service.sh
+++ service/service.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 echo "starting service"
 touch /env/.env && chmod 777 /env/.env
-while true
-do
-    sleep 5s
-    cat /env/.env
-    date
-done
+if grep -q "0" "/env/.env";
+then
+    echo "0 found"
+    while true
+    do
+        sleep 10s
+        cat /env/.env
+        date
+    done
+else
+    echo "0 not found :("
+    exit 1
+fi
--- updater/watcher.sh
+++ updater/watcher.sh
@@ -4,7 +4,6 @@ count=0
 while true
 do
     sleep 5s
-    echo "list"
     echo $count >> /env/.env
     count=`expr $count + 1`
 done
